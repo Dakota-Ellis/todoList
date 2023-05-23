28c666797048231e08a08b6488bbd9693b997204
Dakota-Ellis <dakotaellis2040@gmail.com>
2023-05-23 17:15:34 -0400

    Can add one Item to list. Need to restructure by removing hardcode input

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const txt1 = document.getElementById('add');
 const btn1 = document.getElementById('btn1');
 const out1 = document.getElementById('output1');
 
+let shoppingList = []
 function addingToList() {
-    out1.innerHTML = txt1.value;
+    out1.innerHTML = txt1.value; 
 }
-
+    
 btn1.addEventListener('click', addingToList);
